d9881205e6943c258756edb1ff3328690f33b7ff
anakorellana <anakorellanasilva@gmail.com>
2023-02-28 11:26:08 -0600

    upload restapp

---
+++ components/checkoutalert.js
@@ -0,0 +1,14 @@
+import Alert from 'react-bootstrap/Alert';
+
+function Alertcheckout() {
+  return (
+    <Alert variant="success">
+  
+      <p>
+Thank you for your order!
+     </p>
+    </Alert>
+  );
+}
+
+export default Alertcheckout;
